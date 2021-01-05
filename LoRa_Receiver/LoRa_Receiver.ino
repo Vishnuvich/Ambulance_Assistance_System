693f0599f4f276db45dde157e25e167a2ccf163a
Vishnu K S <vishnuksvichu12345@gmail.com>
2021-01-06 02:15:37 +0530

    Added different part of codes to user defined functions

--- LoRa_Receiver/LoRa_Receiver.ino
+++ LoRa_Receiver/LoRa_Receiver.ino
@@ -5,6 +5,7 @@
 float latitude,longitude;
 int distance;
 char charreceived[32];
+String received, received_data;
 //Pole location
 const float fixedlat = 10.113079;//30.236641; 
 const float fixedlong = 76.351358;//-97.821457;
@@ -21,26 +22,31 @@ void setup() {
     while (1);
   }
 }
+/*--------------------------LoRa Receive Function----------------------------*/
 
-void loop() {
-   int a = 0;
-  // try to parse packet
-  int packetSize = LoRa.parsePacket();
-  if (packetSize) {
-    // read packet
-    while (LoRa.available()) {
+String LoRa_Receive()
+{
+  int a = 0;
+  // read packet
+      while (LoRa.available()) 
+    {
        char receiv = LoRa.read();//receives one character at a time(iteration)
        //storing each char to an array
        charreceived[a]=receiv;
        a++;
     }
-    
     //Array to string
-    String received = charreceived;
-    Serial.println(received);
+    received = charreceived; //here the variable "received" is a string type
+    return received;
+}
+
+/*--------------------------------Fuction for distance checking and extracting lat and long values--------------------------*/
+
+void distance_Check(String received_data)
+{
     //splitting received string and converting to float
-    latitude =received.substring(0,9).toFloat();
-    longitude = received.substring(9).toFloat();
+    latitude =received_data.substring(0,9).toFloat();
+    longitude = received_data.substring(9).toFloat();
     Serial.print("Latitude : ");
     Serial.println(latitude,6);
     Serial.print("Longitude :");
@@ -53,8 +59,18 @@ void loop() {
           Serial.println(distance);
           Serial.println("Ambulance passed the pollling station");
         }
+}
+
+
+void loop() {
+   
+  // try to parse packet
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    received_data = LoRa_Receive();//calling user defined lora receive function which returns the received string
+    Serial.println(received_data);
     
-    
+    distance_Check(received_data);
       
     // print RSSI of packet
     Serial.print("' with RSSI ");
