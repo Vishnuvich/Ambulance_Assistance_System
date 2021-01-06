08b168c93b6bf2ad9f582f2e0be0dfd0a8b82a10
Vishnu K S <vishnuksvichu12345@gmail.com>
2021-01-06 17:12:01 +0530

    Made distance check function to true if condition satisfied

--- LoRa_Receiver/LoRa_Receiver.ino
+++ LoRa_Receiver/LoRa_Receiver.ino
@@ -1,7 +1,7 @@
 #include <SPI.h>
 #include <LoRa.h>
 #include <TinyGPS++.h>
-
+/*-------------------------------------------------------Variables---------------------------------------------------------------*/
 float latitude,longitude;
 int distance;
 char charreceived[32];
@@ -10,18 +10,6 @@ String received, received_data;
 const float fixedlat = 10.113079;//30.236641; 
 const float fixedlong = 76.351358;//-97.821457;
 
-
-void setup() {
-  Serial.begin(9600);
-  while (!Serial);
-
-  Serial.println("LoRa Receiver");
-  LoRa.setPins(15,16,4);
-  if (!LoRa.begin(473E6)) {
-    Serial.println("Starting LoRa failed!");
-    while (1);
-  }
-}
 /*--------------------------------------------------LoRa Receive Function----------------------------------------------------------*/
 
 String LoRa_Receive()
@@ -42,7 +30,7 @@ String LoRa_Receive()
 
 /*--------------------------------Fuction for distance checking and extracting lat and long values--------------------------------*/
 
-void distance_Check(String received_data)
+int distance_Check(String received_data)
 {
     //splitting received string and converting to float
     latitude =received_data.substring(0,9).toFloat();
@@ -54,23 +42,42 @@ void distance_Check(String received_data)
     //Distance calculation, returns distance in meter
     distance = TinyGPSPlus::distanceBetween(latitude,longitude,fixedlat,fixedlong);
       Serial.println(distance);
-      if (distance < 100 ) //give the difference in distance here
+      if (distance < 123 ) //give the difference in distance here
         {
           Serial.println(distance);
           Serial.println("Ambulance passed the pollling station");
+          return true;/* returns true if the condition is satisfied ie, when the ambulance is within the specified range 
+                       of station or we can say that the ambulance crossed the polling station*/
         }
 }
 
+/*----------------------------------------------------Arduino Default Functions---------------------------------------------------------*/
 
-void loop() {
-   
-  // try to parse packet
-  int packetSize = LoRa.parsePacket();
-  if (packetSize) {
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  Serial.println("LoRa Receiver");
+  LoRa.setPins(15,16,4);
+  if (!LoRa.begin(473E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+}
+
+
+void loop() 
+{   
+  int packetSize = LoRa.parsePacket();// try to parse packet
+  if (packetSize) 
+  {
     received_data = LoRa_Receive();//calling user defined lora receive function which returns the received string
     Serial.println(received_data);
-    //Checking the distance between ambulance and polling station
-    distance_Check(received_data);
+    /*Checking the distance between ambulance and polling station,if it is less than specified value the function returns "true"*/
+    if (distance_Check(received_data)== true)
+    {
+     Serial.println("Works"); 
+    }
     // print RSSI of packet
     Serial.print("' with RSSI ");
     Serial.println(LoRa.packetRssi());
