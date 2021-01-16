44ba7b2a90fa79466da35a05eabfa552071f9503
Vishnu K S <vishnuksvichu12345@gmail.com>
2021-01-17 01:45:54 +0530

    Added code for GPS in sender

--- LoRa_Sender/LoRa_Sender.ino
+++ LoRa_Sender/LoRa_Sender.ino
@@ -1,7 +1,11 @@
 #include <SPI.h>
 #include <LoRa.h>
 #include <TinyGPS++.h>
+#include <SoftwareSerial.h>
 
+//For GPS Pins
+static const int RXPin = 4, TXPin = 5;
+static const uint32_t GPSBaud = 9600;
 
 //Variables
 
@@ -12,6 +16,8 @@ String longitude, latitude;
 // The TinyGPS++ object
 TinyGPSPlus gps;
 
+// The serial connection to the GPS device
+SoftwareSerial ss(RXPin, TXPin);
 
 //Display Function
 
@@ -43,7 +49,8 @@ void displayInfo()
 
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  ss.begin(GPSBaud);
   while (!Serial);
 
   Serial.println("LoRa Sender");
@@ -57,11 +64,20 @@ void setup() {
 }
 
 void loop() {
-  
-   const char *gpsStream = "$GPRMC,045103.000,A,3014.1984,N,09749.2872,W,0.67,161.46,030913,,,A*7C\r\n";
-   while(*gpsStream)
-    if (gps.encode(*gpsStream++)){
+
+    while (ss.available() > 0)
+    if (gps.encode(ss.read()))
       displayInfo();
-    }
+      if (millis() > 5000 && gps.charsProcessed() < 10)
+  {
+    Serial.println(F("No GPS detected: check wiring."));
     delay(3000);
-}
+  }
+  }
+//   const char *gpsStream = "$GPRMC,045103.000,A,3014.1984,N,09749.2872,W,0.67,161.46,030913,,,A*7C\r\n";
+//   while(*gpsStream)
+//    if (gps.encode(*gpsStream++)){
+//      displayInfo();
+//    }
+//    delay(3000);
+//}
